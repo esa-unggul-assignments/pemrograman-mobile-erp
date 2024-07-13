8b6db357572d0b9649041603d81ad6e85b8230fb
Muhammad Jihad Robbani <62271731+jihadrobb@users.noreply.github.com>
2024-07-13 22:29:30 +0700

    remove old app

--- custom_addons/user_auth/models/user.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class User(models.Model):
-    _name = 'user.auth'
-    _description = 'User Authentication'
-
-    email = fields.Char(string='Email', required=True, unique=True)
-    password = fields.Char(string='Password', required=True)
--- erp_mobile/android/app/src/main/kotlin/com/example/erp_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.erp_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
